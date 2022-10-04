4e5b263dc5c8d72021782c030f7ca8412298286e
rotoapanta <robertocarlos.toapanta@gmail.com>
2022-10-04 16:40:06 -0500

    Deleted wifi credentials

--- wifi_credentials.py
+++ wifi_credentials.py
@@ -1,11 +1,2 @@
-#ssid = 'RCAG'  #Nombre de la Red
-#password = '1984@1986' #Contraseña de la red
-
-#ssid = 'Electronica'  #Nombre de la Red
-#password = '16E0F151C0' #Contraseña de la red
-
-#ssid = 'rotoapanta'  #Nombre de la Red
-#password = 'admin12345' #Contraseña de la red
-
-ssid = 'LGRC'  #Nombre de la Red
-password = '2706@09rg' #Contraseña de la red
+ssid = 'YOUR_SSID'  # Your network SSID (name)
+password = 'YOUR_SSID_PASSWORD' # your network password (use for WPA, or use as key for WEP)
