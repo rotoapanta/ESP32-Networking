1efab966db5b734f120b4dfce58239ba95e43d66
rotoapanta <robertocarlos.toapanta@gmail.com>
2022-10-06 15:25:28 -0500

    Modified wifi_credentials.py

--- wifi_credentials.py
+++ wifi_credentials.py
@@ -1,2 +1,21 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+"""
+__file__ = wifi_credentials.py
+__author__ = rotoapanta "Roberto Toapanta"
+__copyright__ = "Copyright 2022, BitTech"
+__credits__ = ["Roberto Toapanta, Giovanny Toapanta"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Roberto Toapanta"
+__email__ = "robertocarlos.toapanta@gmail.com"
+__status__ = "Production"
+__date__ = 16/9/22 10:58
+__description__ = "This project is based on an ESP32-S2 general-purpose development board 
+                    with micropython to connect to the local network. "
+__information__ : 
+"""
+
 ssid = 'YOUR_SSID'  # Your network SSID (name)
 password = 'YOUR_SSID_PASSWORD' # your network password (use for WPA, or use as key for WEP)
